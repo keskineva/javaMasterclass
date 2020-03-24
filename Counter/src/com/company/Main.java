21f26a441fc0ed0b5fb53acd0ae25b0d3f388c98
keskineva <developer@example.com>
2020-03-24 16:18:40 +0200

    uploading Messages from Thread topic

--- Counter/src/com/company/Main.java
+++ Counter/src/com/company/Main.java
@@ -1,8 +1,55 @@
 package com.company;
 
+import static com.company.ThreadColor.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Countdown countdown = new Countdown();
+
+        CountDownThread t1 = new CountDownThread(countdown);
+        t1.setName("Thread 1");
+        CountDownThread t2 = new CountDownThread(countdown);
+        t1.setName("Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+class Countdown {
+
+    private int i;
+
+    public void doCountdown() {
+        String color;
+        switch (Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ANSI_CYAN;
+                break;
+            case "Thread 2":
+                color = ANSI_PURPLE;
+                break;
+            default:
+                color = ANSI_GREEN;
+        }
+
+        synchronized (this) {
+            for (i = 10; i > 0; i--) {
+                System.out.println(color + Thread.currentThread().getName() + ": i=" + i);
+            }
+        }
+    }
+}
+
+class CountDownThread extends Thread {
+    private Countdown threadCountdown;
+
+    public CountDownThread(Countdown countdown) {
+        threadCountdown = countdown;
+    }
+
+    public void run() {
+        threadCountdown.doCountdown();
     }
 }
